3fa1fcdc0f4fac3b88ab869676e6171cb4c871e2
Marlon Hernández <mmarlon.ssantiago@gmail.com>
2026-05-05 17:31:41 -0500

    Changed astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: "/chernitovets.github.io/",
   vite: {
     plugins: [
       tailwindcss()
